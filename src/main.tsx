cb06364e5447e4190a97eea7a5722a75055e88b2
abbasmah <abbas.shah89@gmail.com>
2026-07-18 03:55:12 +0500

    Add build-time pre-rendering (vite-react-ssg), broaden schema.org, add llms.txt, freshen sitemap dates, homepage bridge accent color

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
-import { StrictMode, lazy, Suspense } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { ViteReactSSG } from 'vite-react-ssg';
 import './index.css';
-import App from './App.tsx';
+import { routes } from './routes';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-);
+export const createRoot = ViteReactSSG({ routes, basename: '/' });
